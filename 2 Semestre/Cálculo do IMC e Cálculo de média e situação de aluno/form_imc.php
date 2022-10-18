cffb7924a4ab6712308037cc68790a3b4331b7e1
Gabriel Rocha <101814874+GabrielTrindade20@users.noreply.github.com>
2022-10-18 18:59:00 -0300

    Add files via upload

---
+++ 2 Semestre/Cálculo do IMC e Cálculo de média e situação de aluno/form_imc.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<center><fieldset>Cadastro
+
+   <br> <form action="resultadoIMC.php" method="post"><br>
+
+            <label for="Nome">Qual o seu peso?</label>
+            <input type="text" name="peso" id="nome1" required autofocus>
+<br><br>
+            <label for="nota">Qual a sua altura</label>
+            <input type="text" name="altura" placeholder="em metros" require>
+            
+<br><br>
+
+            <input type="submit" value="enviar" name="enviar"></center>
+
+    
+</body>
+</html>
